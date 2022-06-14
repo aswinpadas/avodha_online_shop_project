7d35148d64457fe13d651d4fd4c8a745efd8a8b4
aswin <aswinpdas83@gmail.com>
2022-06-14 19:58:57 +0530

    database models.py created; for cart and products

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,19 @@
+from datetime import datetime
+
 from django.db import models
 
 # Create your models here.
+from django.db.models import CASCADE
+from shop.models import products, categ
+
+
+class cart(models.Model):
+    cart_id=models.CharField(max_length=300,unique=True)
+    date=models.DateField(auto_now_add=True)
+    product_name=models.CharField(max_length=300)
+    categName=models.ForeignKey(categ,on_delete=CASCADE)
+    product_qty=models.IntegerField()
+    product_id=models.ForeignKey(products,on_delete=CASCADE)
+
+    def __str__(self):
+        return self.cart_id
