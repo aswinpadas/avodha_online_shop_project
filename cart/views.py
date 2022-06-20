1a244d45602214810add3df73d5581b8a09220fb
aswin <aswinpdas83@gmail.com>
2022-06-20 21:03:05 +0530

    Todays Commit: *  cart

--- cart/views.py
+++ cart/views.py
@@ -3,11 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 from shop.models import Products, Cart
 
+def createCartId(req):
+    c_id=req.session_key
+    if not c_id:
+        c_id=req.session.create()
+    return c_id
 
 def addToCart(req, slug):
-    prod_obj=Products.objects.get(slug=slug)
-    cart_obj = Cart()
-    cart_obj.product_id=prod_obj.slug
-    cart_obj.qty=1
-    cart_obj.save()
+    try:
+
+        cart_obj = Cart()
+        cart_obj.product_id=prod_obj.slug
+        cart_obj.qty=1
+        cart_obj.save()
     return render(req,'cart.html',)
