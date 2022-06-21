c7b14aef1ada3d71fe21dd9bdffd9f628a23de7d
aswin <aswinpdas83@gmail.com>
2022-06-21 11:39:40 +0530

    Todays Commit: *  cart

--- cart/views.py
+++ cart/views.py
@@ -1,17 +1,31 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-
+from . models import cart
 # Create your views here.
 from shop.models import Products, Cart
 
 def createCartId(req):
-    c_id=req.session_key
-    if not c_id:
+    if not req.session.exists(req.session.session_key):
+        req.session.create()
         c_id=req.session.create()
-    return c_id
+        return c_id
 
 def addToCart(req, slug):
     try:
-        pass
+        prod_obj=Products.objects.get(slug=slug)
+        cart_obj= cart.objects.get(cart_id=createCartId(req))
+        cart_obj.product_id = slug
+        cart_obj.product_name = prod_obj.product_name
+        cart_obj.save()
+        if not cart_obj:
+            cart_obj=cart()
+            cart_obj.cart_id=createCartId(req)
+            cart_obj.product_id=slug
+            cart_obj.product_name=prod_obj.product_name
+            cart_obj.save()
+        return HttpResponse('Cart Added')
+
     except:
-        pass
-    return render(req,'cart.html',)
+        return HttpResponse('Cart not Added')
+
+    # return render(req,'cart.html',)
